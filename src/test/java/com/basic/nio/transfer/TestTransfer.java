d2ef6e9b381c9c8fccbd3b029c57259ef5e8bd50
dell-pc <798750509@qq.com>
2017-09-24 20:49:37 +0800

    Initial commit

---
+++ src/test/java/com/basic/nio/transfer/TestTransfer.java
@@ -0,0 +1,91 @@
+package com.basic.nio.transfer;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Iterator;
+
+/**
+ * locate com.basic.nio.transfer
+ * Created by 79875 on 2017/9/24.
+ */
+public class TestTransfer {
+
+    @Test
+    public void testTransferToClient() throws IOException {
+        FileChannel inChannel = FileChannel.open(Paths.get("d:/CHANGES.txt"), StandardOpenOption.READ);
+        SocketChannel sChannel = SocketChannel.open(new InetSocketAddress("localhost", 9898));
+        inChannel.transferTo(0,1024*1024,sChannel);
+        sChannel.shutdownOutput();
+    }
+
+    @Test
+    public void testTransferToServerBlocking() throws IOException {
+        ServerSocketChannel ssChannel = ServerSocketChannel.open();
+
+        ssChannel.bind(new InetSocketAddress(9898));
+
+        SocketChannel sChannel = ssChannel.accept();
+
+        ByteBuffer buf = ByteBuffer.allocate(1024);
+
+        while(sChannel.read(buf) != -1){
+            buf.flip();
+            System.out.println(new String(buf.array(),0,buf.limit()));
+            buf.clear();
+        }
+
+        sChannel.close();
+        ssChannel.close();
+    }
+
+    @Test
+    public void testTransferToServerNonBlocking() throws IOException {
+        ServerSocketChannel ssChannel = ServerSocketChannel.open();
+
+        ssChannel.configureBlocking(false);
+
+        ssChannel.bind(new InetSocketAddress(9898));
+
+        Selector selector = Selector.open();
+
+        ssChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        while(selector.select() > 0){
+
+            Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+
+            while(it.hasNext()){
+                SelectionKey sk = it.next();
+
+                if(sk.isAcceptable()){
+
+                    SocketChannel sChannel = ssChannel.accept();
+
+                    sChannel.configureBlocking(false);
+
+                    sChannel.register(selector, SelectionKey.OP_READ);
+                }else if(sk.isReadable()){
+
+                    SocketChannel sChannel = (SocketChannel) sk.channel();
+
+                    ByteBuffer buf = ByteBuffer.allocate(1024);
+
+                    int len = 0;
+                    while((len = sChannel.read(buf)) > 0 ){
+                        buf.flip();
+                        System.out.println(new String(buf.array(), 0, len));
+                        buf.clear();
+                    }
+                }
+
+                it.remove();
+            }
+        }
+    }
+}
